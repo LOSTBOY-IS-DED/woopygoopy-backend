008bc032ca0e0f428b7c9fb07857dea87239c266
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2025-05-04 01:48:38 +0530

    new linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,14 @@
-import tseslint from 'typescript-eslint';
+import pkg from '@typescript-eslint/eslint-plugin';
+const eslintPlugin = pkg;  // Directly assign the plugin package
+import parser from '@typescript-eslint/parser';
 
-export default tseslint.config({
+export default {
   files: ['**/*.ts'],
   languageOptions: {
-    parser: tseslint.parser,
+    parser,
   },
   plugins: {
-    '@typescript-eslint': tseslint.plugin,
+    '@typescript-eslint': eslintPlugin,  // Correctly assign the plugin here
   },
   rules: {
     '@typescript-eslint/explicit-function-return-type': 'warn',
@@ -14,4 +16,4 @@ export default tseslint.config({
     '@typescript-eslint/no-explicit-any': 'warn',
     '@typescript-eslint/ban-ts-comment': 'warn',
   },
-});
+};
